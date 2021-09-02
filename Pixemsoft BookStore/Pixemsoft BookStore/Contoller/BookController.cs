70e2da8c2f7fdb05dda0c21ce321808ad744ffcf
FahidZahoorfreelancer <fahidfreelancer@gmail.com>
2021-09-02 09:52:35 +0500

    After adding MVC with jaquery and bootstrap

---
+++ Pixemsoft BookStore/Pixemsoft BookStore/Contoller/BookController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pixemsoft_BookStore.Model;
+using Pixemsoft_BookStore.Model.Repositroy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pixemsoft_BookStore.Contoller
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookrepository=null;
+        public BookController()
+        {
+            _bookrepository = new BookRepository();
+        }
+        public ViewResult GetAllBooks()
+        {
+            var data= _bookrepository.getallBooks();
+            return View();
+        }
+        public BookModel getabooks(int id)
+        {
+            return _bookrepository.GetbookbyID(id);
+        }
+        public List<BookModel> searchbook(string bokname, string authothername)
+        {
+            return _bookrepository.searchbook(bokname, authothername);
+        }
+    }
+}
---
+++ Pixemsoft BookStore/Pixemsoft BookStore/Model/Repositroy/BookRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pixemsoft_BookStore.Model.Repositroy
+{
+    public class BookRepository
+    {
+        public List<BookModel> getallBooks()
+        {
+            return datasource();
+        }
+        public BookModel GetbookbyID(int ID)
+        {
+            return datasource().Where(x => x.id == ID).FirstOrDefault();
+        }
+        public List<BookModel> searchbook(string title, string author)
+        {
+            return datasource().Where(x => x.Title == title || x.Author == author).ToList();
+        }
+        private List<BookModel> datasource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){ id=1,Title="MVC",Author="Javed"},
+                 new BookModel(){ id=2,Title="Englis",Author="Ali"},
+                  new BookModel(){ id=3,Title="Math",Author="hader"},
+                   new BookModel(){ id=4,Title="Computer",Author="fahad"},
+                    new BookModel(){ id=5,Title="Java",Author="khan"},
+            };
+        
+        }
+    }
+}
